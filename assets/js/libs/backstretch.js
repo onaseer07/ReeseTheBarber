1544b17a651014e82052d331915722c1859747aa
onaseer07 <onaseer07@gmail.com>
2018-12-21 12:33:27 -0600

    fixed the style of header

--- assets/js/libs/backstretch.js
+++ assets/js/libs/backstretch.js
@@ -1,5 +1,8 @@
-$('#carousel').backstretch([
-    "images/outside.jpg"
-  , "images/garfield-interior.jpg"
-  , "images/cheers.jpg"
-], {duration: 3000, fade: 750});
+// $('#carousel').backstretch([
+//     "images/outside.jpg"
+//   , "images/garfield-interior.jpg"
+//   , "images/cheers.jpg"
+// ], {duration: 3000, fade: 750});
+
+$("#main").backstretch('assets/images/woodBG.jpg');
+console.log('backstretch is configured');
